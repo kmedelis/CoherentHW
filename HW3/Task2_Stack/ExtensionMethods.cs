b36733f253e3a8081f30c83090be41ebb1e1e09b
kmedelis <84917319+kmedelis@users.noreply.github.com>
2022-02-06 15:38:54 +0200

    fixes to the stack task

--- HW3/Task2_Stack/ExtensionMethods.cs
+++ HW3/Task2_Stack/ExtensionMethods.cs
@@ -5,28 +5,14 @@ namespace Task2_Stack_Extension
 {
     public static class ExtensionMethods
     {
-        public static MyStack<T> ReverseStack<T>(this MyStack<T> stackToReverse)
+        public static Stack<T> ReverseStack<T>(this IStack<T> stackToReverse)
         {
-            var temp = new List<T>(); // using list to reverse stack
-            MyStack<T> reversedStack = new MyStack<T>(); // this is the stack to return
-            MyStack<T> tempStack = new MyStack<T>(); // this is a temporary stack
-
-            while (stackToReverse.index > 0)
+            Stack<T> tempStack = new Stack<T>();
+            while (!stackToReverse.IsEmpty())
             {
-                var tempRemove = stackToReverse.Pop(); //remove from original stack
-                temp.Add(tempRemove); // add to the list
-                tempStack.Push(tempRemove); // push from the temporary stack
-
+                tempStack.Push(stackToReverse.Pop());
             }
-
-            foreach (T i in temp)
-            {
-                stackToReverse.Push(tempStack.Pop()); // we do this so that the original stackToReverse becomes not empty
-                reversedStack.Push(i); // we do this to reverse the stack and then we return it.
-            }
-
-            return reversedStack;
+            return tempStack;
         }
     }
-
 }
--- HW3/Task2_Stack/Stack.cs
+++ HW3/Task2_Stack/Stack.cs
@@ -6,52 +6,45 @@ namespace Task2_Stack
 {
     public class MyStack<T> : IStack<T>
     {
-        public T[] arrayStack { get; set; }
+        private T[] _arrayStack { get; set; }
         private int _stackSize;
-        public int index;
+        private int _index;
 
 
         public MyStack()
         {
             _stackSize = 1;
-            int index = 0;
-            arrayStack = new T[_stackSize];
+            int _index = 0;
+            _arrayStack = new T[_stackSize];
         }
 
         public bool IsEmpty()
         {
-            if (index == 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return _index == 0;
         }
 
         public T Pop()
         {
-            if (index == 0)
+            if (_index == 0)
             {
                 throw new InvalidOperationException("Exception: Empty stack"); // don't know much about exceptions yet, but I think this is apropriate use of them.
             }
-            index--;
-            T temp = arrayStack[index];
+            _index--;
+            T temp = _arrayStack[_index];
             return temp;
         }
 
         public void Push(T obj)
         {
-            if (index == _stackSize)
+            if (_index == _stackSize)
             {
                 _stackSize = _stackSize * 2;
                 T[] tempArray = new T[_stackSize];
-                Array.Copy(arrayStack, 0, tempArray, 0, index);
-                arrayStack = tempArray;
+                Array.Copy(_arrayStack, 0, tempArray, 0, _index);
+                _arrayStack = tempArray;
             }
-            arrayStack[index] = obj;
-            index++;
+            _arrayStack[_index] = obj;
+            _index++;
         }
     }
 }
