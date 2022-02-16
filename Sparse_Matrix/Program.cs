381d54028100b92d0e447a50c2891c1eadda1a52
kmedelis <84917319+kmedelis@users.noreply.github.com>
2022-02-16 13:18:03 +0200

    small fix to SparseMatrix

---
+++ Sparse_Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sparse_Matrix;
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using Sparse_Matrix;
+using System.Diagnostics;
+
+SparseMatrix sparseMatrix = new SparseMatrix(6,6);
+
+sparseMatrix[4, 1] = 5;
+sparseMatrix[0, 4] = 2;
+sparseMatrix[0, 0] = 3;
+
+
+foreach(int sparse in sparseMatrix)
+{
+    Console.WriteLine(sparse); // returns the correct values
+}
+
+Console.WriteLine(sparseMatrix.ToString()); // prints out the matrix correctly
+
+Console.WriteLine(sparseMatrix.GetCount(3)); // returns 1
+Console.WriteLine(sparseMatrix.GetCount(0)); // returns 33
+
+foreach ((int, int, int) element in sparseMatrix.GetNozeroElements()) //is this the correct implementation? it definitely works, but I am not sure whether it's correct
+{
+    Console.WriteLine(element); 
+}
+
---
+++ Sparse_Matrix/SparseMatrixEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Sparse_Matrix
+{
+	public class SparseMatrixEntry
+	{
+		public int CoordinateX;
+		public int CoordinateY;
+		public int Value;
+
+		public SparseMatrixEntry(int coordinateX, int coordinateY, int value)
+		{
+			CoordinateX = coordinateX;
+			CoordinateY = coordinateY;
+			Value = value;
+		}
+	}
+}
+
